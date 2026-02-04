46475f1a02dca4eb837b54f4a9651f97f0029449
UnchiBurio <tegurudaisuki@gmail.com>
2026-02-05 07:24:14 +0900

    Update DraggableNavigator.gd

--- DraggableNavigator.gd
+++ DraggableNavigator.gd
@@ -2,33 +2,58 @@
 extends TextureRect
 
 var dragging := false
+var resizing := false
 var drag_start_position := Vector2.ZERO
+var resize_start_position := Vector2.ZERO
+var resize_start_size := Vector2.ZERO
 var border_color := Color.WHITE
 var border_width := 2.0
+var resize_handle_size := 12.0
+var min_size := Vector2(20, 20)
 
 func _ready():
 	# マウス入力を受け付けるように設定
 	mouse_filter = Control.MOUSE_FILTER_PASS
+	expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	stretch_mode = TextureRect.STRETCH_SCALE
 	gui_input.connect(_on_gui_input)
 
 func _draw():
 	# 枠線を描画
 	var rect = Rect2(Vector2.ZERO, size)
 	draw_rect(rect, border_color, false, border_width)
+	# リサイズハンドル
+	var handle_rect = Rect2(size - Vector2(resize_handle_size, resize_handle_size), Vector2(resize_handle_size, resize_handle_size))
+	draw_rect(handle_rect, border_color, true)
 
 func _on_gui_input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				# ドラッグ開始
-				dragging = true
-				drag_start_position = get_global_mouse_position() - position
+				# ドラッグ/リサイズ開始
+				if _is_on_resize_handle(event.position):
+					resizing = true
+					resize_start_position = get_global_mouse_position()
+					resize_start_size = size
+				else:
+					dragging = true
+					drag_start_position = get_global_mouse_position() - position
 			else:
-				# ドラッグ終了
+				# ドラッグ/リサイズ終了
 				dragging = false
+				resizing = false
 	
 	elif event is InputEventMouseMotion:
-		if dragging:
+		if resizing:
+			var mouse_delta = get_global_mouse_position() - resize_start_position
+			var new_size = resize_start_size + mouse_delta
+			var viewport_size = get_viewport_rect().size
+			var max_size = viewport_size - position
+			new_size.x = clamp(new_size.x, min_size.x, max_size.x)
+			new_size.y = clamp(new_size.y, min_size.y, max_size.y)
+			size = new_size
+			queue_redraw()
+		elif dragging:
 			# ドラッグ中の位置更新
 			position = get_global_mouse_position() - drag_start_position
 			
@@ -36,3 +61,6 @@ func _on_gui_input(event: InputEvent):
 			var viewport_size = get_viewport_rect().size
 			position.x = clamp(position.x, 0, viewport_size.x - size.x)
 			position.y = clamp(position.y, 0, viewport_size.y - size.y)
+
+func _is_on_resize_handle(local_pos: Vector2) -> bool:
+	return local_pos.x >= size.x - resize_handle_size and local_pos.y >= size.y - resize_handle_size
